b44f4198b3c7b0def9d84635bc376dc3963c265d
LOZONSCHI CONSTANTIN-RAZVAN <310440501RSL211122@feaa.uaic.ro>
2022-10-17 08:44:52 +0300

    Add project files.

---
+++ lab 3/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.buttonSolution = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxA.Location = new System.Drawing.Point(166, 68);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(145, 29);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxB.Location = new System.Drawing.Point(166, 145);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(145, 29);
+            this.textBoxB.TabIndex = 3;
+            // 
+            // buttonSolution
+            // 
+            this.buttonSolution.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSolution.Location = new System.Drawing.Point(166, 242);
+            this.buttonSolution.Name = "buttonSolution";
+            this.buttonSolution.Size = new System.Drawing.Size(94, 29);
+            this.buttonSolution.TabIndex = 4;
+            this.buttonSolution.Text = "Check";
+            this.buttonSolution.UseVisualStyleBackColor = true;
+            this.buttonSolution.Click += new System.EventHandler(this.buttonSolution_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSolution);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Button buttonSolution;
+    }
+}
+
---
+++ lab 3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSolution_Click(object sender, EventArgs e)
+        {
+            double a, b, result;
+            try
+            {
+                a = Double.Parse(textBoxA.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("'A' should be a double number");
+                return;
+            }
+            try
+            {
+                b = Double.Parse(textBoxB.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("'B' should be a double number");
+                return;
+            }
+            if (a == 0)
+                if (b == 0)
+                    MessageBox.Show("There is an infinitive solution");
+                else
+                    MessageBox.Show("No solution");
+            else
+            {
+                result = -b / a;
+                MessageBox.Show("Result" + result.ToString());
+            }
+        }
+    }
+}
